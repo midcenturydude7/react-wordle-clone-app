60a27fb663f75363e8982a4bc9f0e38d37312eb1
midCenturyDude7 <mgriffes@gmail.com>
2023-02-02 17:11:53 -0800

    Working on Excerise #4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -20,7 +20,7 @@ function Game() {
 
   return (
     <>
-      <GuessRender guesses={guesses} />
+      <GuessRender guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
